8c9ff91aa7a6361f05a266453b35a9e9ae42a6e2
nicodmt <dumont.nicolas.94@gmail.com>
2020-11-07 00:30:54 +0100

    Create SpringMvcConfig class

--- src/contact/config/SpringMvcConfig.java
+++ src/contact/config/SpringMvcConfig.java
@@ -1,5 +1,47 @@
 package contact.config;
 
-public class SpringMvcConfig {
+import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import contact.dao.ContactDAO;
+import contact.dao.ContactDAOImpl;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "contact")
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+	@Bean
+	public DataSource getDataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+
+		return dataSource;
+	}
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+	
+	@Bean
+	public ContactDAO getContactDAO() {
+		return new ContactDAOImpl(getDataSource());
+	}
 }
