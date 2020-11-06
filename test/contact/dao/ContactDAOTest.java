7962c4fc61d445e8519f97068d1453f0d149256d
nicodmt <dumont.nicolas.94@gmail.com>
2020-11-06 23:25:02 +0100

    Create test for save() to store in DB

--- test/contact/dao/ContactDAOTest.java
+++ test/contact/dao/ContactDAOTest.java
@@ -3,12 +3,29 @@ package contact.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import contact.model.Contact;
 
 class ContactDAOTest {
+	
+	private DriverManagerDataSource dataSource;
+	private ContactDAO dao;
 
 	@Test
 	void testSave() {
-		fail("Not yet implemented");
+		dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		
+		dao = new ContactDAOImpl(dataSource);
+		
+		Contact contact = new Contact("Steve Jobs", "steve@apple.com", "Cupertino, CA", "1700123456789");
+		int result = dao.save(contact);
+		
+		assertTrue(result > 0);
 	}
 
 	@Test
