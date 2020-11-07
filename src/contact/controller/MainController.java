0d0e55cc2566f0a5fff17dda4fab50d3ae58cbe8
nicodmt <dumont.nicolas.94@gmail.com>
2020-11-07 01:22:35 +0100

    Add HTML to show data

--- src/contact/controller/MainController.java
+++ src/contact/controller/MainController.java
@@ -1,13 +1,26 @@
 package contact.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import contact.dao.ContactDAO;
+import contact.model.Contact;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private ContactDAO contactDAO;
+	
 	@RequestMapping(value = "/")
-	public String home() {
-		return "index";
+	public ModelAndView listContact(ModelAndView model) {
+		List<Contact> listContact = contactDAO.list();
+		model.addObject("listContact", listContact);
+		model.setViewName("index");
+		return model;
 	}
 }
